17518641b897444cfbd8b94e1814d5cd90baf0a4
Vishesh Singh <VisheshSingh16@gmail.com>
2020-11-16 20:01:50 -0600

    Text Component

--- App.js
+++ App.js
@@ -3,10 +3,15 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const handlePress = () => {
+    console.log('Text Pressed!');
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text numberOfLines={1} onPress={handlePress}>
+        Open up App.js to start working on your app! This can be even longer and
+        longer which goes beyond one line
+      </Text>
     </View>
   );
 }
